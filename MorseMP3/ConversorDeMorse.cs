41bc76b39bf17ca406d85b9fc7f0568301a2ae16
Matias <matumartinez9@hotmail.com>
2020-06-28 18:19:39 -0300

    Agregado p2

---
+++ MorseMP3/ConversorDeMorse.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Helpers
+{
+    class ConversorDeMorse
+    {
+        static Dictionary<char, string> DiccionarioMorse = new Dictionary<char, string>()
+        {
+            {'A' , ".-"},
+            {'B' , "-..."},
+            {'C' , "-.-."},
+            {'D' , "-.."},
+            {'E' , "."},
+            {'F' , "..-."},
+            {'G' , "--."},
+            {'H' , "...."},
+            {'I' , ".."},
+            {'J' , ".---"},
+            {'K' , "-.-"},
+            {'L' , ".-.."},
+            {'M' , "--"},
+            {'N' , "-."},
+            {'O' , "---"},
+            {'P' , ".--."},
+            {'Q' , "--.-"},
+            {'R' , ".-."},
+            {'S' , "..."},
+            {'T' , "-"},
+            {'U' , "..-"},
+            {'V' , "...-"},
+            {'W' , ".--"},
+            {'X' , "-..-"},
+            {'Y' , "-.--"},
+            {'Z' , "--.."},
+            {'1',".---"},
+            {'2',"..---"},
+            {'3',"...--"},
+            {'4',"....-"},
+            {'5',"....."},
+            {'6',"-...."},
+            {'7',"--..."},
+            {'8',"---.."},
+            {'9',"----."},
+            {'0',"-----"},
+
+        };
+
+
+        public static string TextoAMorse(string recibido)
+        {
+            string resultado = "";
+
+            recibido = recibido.ToUpper();
+            char[] arreglo = recibido.ToCharArray();
+
+            foreach (char item in arreglo)
+            {
+                char aux = item;
+                if (aux == Convert.ToChar(" "))
+                {
+                    resultado += "/";
+                }
+                else
+                {
+                    resultado += " " + DiccionarioMorse[item] + " ";
+                }
+            }
+
+            return resultado;
+        }
+
+
+        public static string MorseATexto(string FraseMorse)
+        {
+            string Texto = "";
+            string[] Letras = FraseMorse.Split(' ');
+            foreach (string item in Letras)
+            {
+                switch (item)
+                {
+                    case ".-":
+                        Texto += 'a';
+                        break;
+                    case "-...":
+                        Texto += 'b';
+                        break;
+                    case "-.-.":
+                        Texto += 'c';
+                        break;
+                    case "-..":
+                        Texto += 'd';
+                        break;
+                    case ".":
+                        Texto += 'e';
+                        break;
+                    case "..-.":
+                        Texto += 'f';
+                        break;
+                    case "--.":
+                        Texto += 'g';
+                        break;
+                    case "....":
+                        Texto += 'h';
+                        break;
+                    case "..":
+                        Texto += 'i';
+                        break;
+                    case ".---":
+                        Texto += 'j';
+                        break;
+                    case "-.-":
+                        Texto += 'k';
+                        break;
+                    case ".-..":
+                        Texto += 'l';
+                        break;
+                    case "--":
+                        Texto += 'm';
+                        break;
+                    case "-.":
+                        Texto += 'n';
+                        break;
+                    case "---":
+                        Texto += 'o';
+                        break;
+                    case ".--.":
+                        Texto += 'p';
+                        break;
+                    case "--.-":
+                        Texto += 'q';
+                        break;
+                    case ".-.":
+                        Texto += 'r';
+                        break;
+                    case "...":
+                        Texto += 's';
+                        break;
+                    case "-":
+                        Texto += 't';
+                        break;
+                    case "..-":
+                        Texto += 'u';
+                        break;
+                    case "...-":
+                        Texto += 'v';
+                        break;
+                    case ".--":
+                        Texto += 'w';
+                        break;
+                    case "-..-":
+                        Texto += 'x';
+                        break;
+                    case "-.--":
+                        Texto += 'y';
+                        break;
+                    case "--..":
+                        Texto += 'z';
+                        break;
+                    case ".----":
+                        Texto += '1';
+                        break;
+                    case "..---":
+                        Texto += '2';
+                        break;
+                    case "...--":
+                        Texto += '3';
+                        break;
+                    case "....-":
+                        Texto += '4';
+                        break;
+                    case ".....":
+                        Texto += '5';
+                        break;
+                    case "-....":
+                        Texto += '6';
+                        break;
+                    case "--...":
+                        Texto += '7';
+                        break;
+                    case "---..":
+                        Texto += '8';
+                        break;
+                    case "----.":
+                        Texto += '9';
+                        break;
+                    case "-----":
+                        Texto += '0';
+                        break;
+                    case "/":
+                        Texto += ' ';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return Texto;
+        }
+
+
+
+        public static void CrearArchivoTAM(string textoT)
+        {
+            string textoM = TextoAMorse(textoT);
+            string fecha = DateTime.Now.ToString("MM-dd-yyyy HH,mm,ss");
+            string nombre = ($"{textoT}_[{fecha}].txt");
+            string DireMorse = @"C:\Repogit\tpn10-MartinezMatiasMaximiliano\MorseMP3\Morse\";
+
+
+            if (!Directory.Exists(DireMorse))
+            {
+                Directory.CreateDirectory(DireMorse);
+                StreamWriter writer = new StreamWriter(File.Open(DireMorse + nombre, FileMode.OpenOrCreate));
+                writer.Write(textoM);
+                writer.Close();
+
+            }
+            else
+            {
+                StreamWriter writer = new StreamWriter(File.Open(DireMorse + nombre, FileMode.Create));
+                writer.Write($"{textoM}");
+                writer.Close();
+
+            }
+        }
+
+        public static string LeerArchivo(string Archivo)
+        {
+            string Directorio = @"C:\Repogit\tpn10-MartinezMatiasMaximiliano\MorseMP3\Morse\";
+            string[] archivosEnDirectorio = Directory.GetFiles(Directorio);
+            string nombreArchivo = "";
+            foreach (string item in archivosEnDirectorio)
+            {
+                if (item.Contains(Archivo))
+                {
+                    string[] nombre = item.Split(@"\");
+                    nombreArchivo = nombre[nombre.Length - 1];
+                }
+            }
+
+
+
+
+            StreamReader reader = new StreamReader(File.Open($"{Directorio}\\{nombreArchivo}", FileMode.Open));
+            string TextoLeido = reader.ReadLine();
+            reader.Close();
+
+            return TextoLeido;
+        }
+
+        public static void CrearArchivoAudio(string Texto)
+        {
+            string Directorio = @"C:\Repogit\tpn10-MartinezMatiasMaximiliano\MorseMP3\Morse\";
+            string texto = ConversorDeMorse.LeerArchivo(Texto);
+            string fecha = DateTime.Now.ToString("MM-dd-yyyy HH,mm,ss");
+
+            string archmp3 = $"{Texto}_[{fecha}].mp3";
+
+
+            string[] PalabrasMorse = texto.Split("/");
+
+            foreach (string palabra in PalabrasMorse)
+            {
+                char[] Letra = palabra.ToCharArray();
+
+
+                using (FileStream stream = File.OpenWrite(Directorio + archmp3))
+                {
+                    foreach (char item in Letra)
+                    {
+                        if (item == '.')
+                        {
+                            byte[] punto = File.ReadAllBytes(@"C:\Repogit\tpn10-MartinezMatiasMaximiliano\MorseMP3\punto.mp3");
+                            stream.Write(punto, 0, punto.Length);
+                            stream.Flush();
+                        }
+                        else if (item == '-')
+                        {
+                            byte[] raya = File.ReadAllBytes(@"C:\Repogit\tpn10-MartinezMatiasMaximiliano\MorseMP3\raya.mp3");
+                            stream.Write(raya, 0, raya.Length);
+                            stream.Flush();
+                        }
+                    }
+
+                    stream.Close();
+                }
+
+
+
+                
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ MorseMP3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using Helpers;
+
+namespace Morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int seleccion = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese opcion:\n [0]Texto a Morse \n [1]Morse a Texto \n [2]Crear Archivo de audio");
+                seleccion = Convert.ToInt32(Console.ReadLine());
+            } while (seleccion < 0 || seleccion > 2);
+
+            Console.WriteLine("");
+
+            if (seleccion == 0)
+            {
+                Console.WriteLine(@"Texto que quiere traducir a morse: ");
+                string TextoInser = Console.ReadLine(); // ... --- ... / 
+                ConversorDeMorse.CrearArchivoTAM(TextoInser);
+            }
+            else if (seleccion == 1)
+            {
+                Console.WriteLine(@"Morse que quiere traducir a texto: ");
+                string MorseInser = Console.ReadLine();
+                string MorseTraducido = ConversorDeMorse.MorseATexto(MorseInser);
+                Console.WriteLine($"{MorseInser}---->{MorseTraducido}");
+            }
+            else
+            {
+                Console.WriteLine(@"Texto que quiere traducir a morse: ");
+                string TextoInser = Console.ReadLine(); // ... --- ... / 
+                ConversorDeMorse.CrearArchivoTAM(TextoInser);
+                string TextoLeido = ConversorDeMorse.LeerArchivo(TextoInser);
+                ConversorDeMorse.CrearArchivoAudio(TextoInser);
+
+
+            }
+
+        }
+    }
+}
